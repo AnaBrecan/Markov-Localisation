b8fb82686bd0ffe8a8c9792feada67e93a7e8e58
Ana-Maria Franzen-Brecan <anabrecan@gmail.com>
2021-10-13 20:54:30 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+//coding exercises following Udacity course on localisation
+#include<iostream>
+#include<vector>
+
+using std::vector;
+
+// initialize priors assuming vehicle at landmark +/- 1.0 meters position stdev
